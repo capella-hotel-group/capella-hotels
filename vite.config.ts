aebb9727bc7841350b1d90bd96fadaecefe0244b
NamitaS123 <107835732+NamitaS123@users.noreply.github.com>
2026-08-31 10:04:05 +0530

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,8 @@ import {
 export default defineConfig({
   resolve: {
     alias: {
-    '@': new URL('./src', import.meta.url).pathname,    },
+      '@': new URL('./src', import.meta.url).pathname,    
+    },
   },
   build: {
     outDir: '.',
